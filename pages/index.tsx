d8ae47f9bd644dc6f31e8949a89e2f551308d4ca
Somraj Mukherjee <iamsomraj@gmail.com>
2021-03-08 13:29:11 +0530

    👌 IMPROVE: added getStaticProps to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import axios from "axios";
 
-const Home: React.FC = () => {
+const Home: React.FC<{ users: string[] }> = ({ users }) => {
   return (
     <div>
       <Head>
@@ -14,9 +15,26 @@ const Home: React.FC = () => {
           content="web dev, web developer, news, react, next news"
         />
       </Head>
-      <h1>Hello World</h1>
+      <h1>Users</h1>
+      <div>
+        {users.map((user) => {
+          return <p key={user}>{user}</p>;
+        })}
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  const users: string[] = data.map((user) => user.name);
+  return {
+    props: {
+      users,
+    },
+  };
+};
